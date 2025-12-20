b0fb73caa76eb2ea94b4c78ae71d0a4987650e9e
FilipK19 <filipkukic15@gmail.com>
2025-12-20 20:00:35 +0100

    added basic playlist download

--- backend/main.py
+++ backend/main.py
@@ -65,3 +65,39 @@ def process_video(data: Url):
 
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
+
+@app.post("/testYT2")
+def process_video(data: Url):
+    # Output folder
+    output_dir = "downloads"
+    os.makedirs(output_dir, exist_ok=True)
+
+    try:
+        ydl_opts = {
+            "quiet": True,
+            "skip_download": False,  # DOWNLOAD the file
+            "format": "bestaudio/best",
+            "outtmpl": f"{output_dir}/%(title)s.%(ext)s",
+            # Convert to MP3
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3",
+                    "preferredquality": "192",
+                }
+            ],
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(data.url, download=True)
+
+        return {
+            "title": info.get("title"),
+            "uploader": info.get("uploader"),
+            "duration": info.get("duration"),
+            "file_saved_as": info.get("title") + ".mp3",
+        }
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
--- frontend/src/app/get-playlist/get-playlist.ts
+++ frontend/src/app/get-playlist/get-playlist.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-get-playlist',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, CommonModule],
   templateUrl: './get-playlist.html',
   styleUrl: './get-playlist.css',
 })
 export class GetPlaylist {
+  url = '';
+  result: any = null;
+  loading = false;
+  error = '';
 
+  constructor(private http: HttpClient) {}
+
+
+  download() {
+    this.loading = true;
+    this.error = '';
+    this.result = null;
+
+    this.http.post<any>('http://127.0.0.1:8000/testYT2', {
+      url: this.url
+    }).subscribe({
+      next: res => {
+        this.result = res;
+        this.loading = false;
+        alert('Download completed!');
+      },
+      error: err => {
+        this.loading = false;
+        this.error = err.error?.detail || 'Something went wrong';
+      }
+    });
+  }
 }
+
